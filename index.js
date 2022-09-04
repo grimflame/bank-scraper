1a942d3f5215a688c4b387c3eaa717610ad516d4
grimflame <iconcosmicalpha@gmail.com>
2022-09-04 19:29:55 -0400

    Updated web scraper

---
+++ index.js
@@ -0,0 +1,58 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const cheerioselect = require("cheerio-select");
+const fs = require("fs");
+
+const floridaUrl = "https://www.creditunionsonline.com/credit-unions-in-altamonte-springs-fl.html";
+async function unionInfo(city)
+{
+    try
+    {
+        const { data } = await axios.get(`https://www.creditunionsonline.com/credit-unions-in-${city}-fl.html`);
+        
+        const $ = cheerio.load(data);
+    }
+    catch (Exception)
+    {
+        console.error(Exception);
+    }
+}
+async function cityUnions(city)
+{
+    try 
+    {
+        const { data } = await axios.get(`https://www.creditunionsonline.com/credit-unions-in-${city}-fl.html`)
+        
+        const $ = cheerio.load(data);
+
+        //get all the cities first
+        const names = $(".pSec");
+        const addresses = $(".hoursRow").children().contents().filter(function() 
+        {
+            return this.nodeType === 3;
+        });
+        const unions = [];
+
+        names.each((idx, el) => 
+        {
+            let union = $(el).text();
+            unions.push(union);
+            addresses.each((x, y) => 
+            {
+                if ($(y).parent().parent().parent().parent().parent().first().text().substring(0, union.toString().length) == union.toString())
+                {
+                    let address = $(y).text();
+                    unions.push(address);
+                }
+            })
+        })
+
+        console.dir(unions);
+    }
+    catch (Exception)
+    {
+        console.error(Exception);
+    }
+}
+
+cityUnions("winter-springs");
